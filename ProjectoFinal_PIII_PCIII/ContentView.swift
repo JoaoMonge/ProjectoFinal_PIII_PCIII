637c230bf6c2160f85700f75b047a75dcec4b90c
JoaoMonge <joao.monge13@gmail.com>
2021-01-05 13:16:54 +0000

    Initial Commit

---
+++ ProjectoFinal_PIII_PCIII/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectoFinal_PIII_PCIII
+//
+//  Created by João Monge on 05/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectoFinal_PIII_PCIII/ProjectoFinal_PIII_PCIIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectoFinal_PIII_PCIIIApp.swift
+//  ProjectoFinal_PIII_PCIII
+//
+//  Created by João Monge on 05/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectoFinal_PIII_PCIIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
